5338110bc2139d0642be89ebb5b187c5d501840b
Yurii Sokolenko <yurasokol3nko@gmail.com>
2019-01-30 19:30:21 +0200

    edit entrypoint.sh

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -71,12 +71,12 @@ fi
 case "$1" in
   webserver)
     airflow initdb
+    echo "Running add_user python script in-case 'airflow' rbac_user is not present. Password is: 'airflow'"
+    python add_user.py
     if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ]; then
       # With the "Local" executor it should all run in one container.
       airflow scheduler &
     fi
-    echo "Running add_user python script in-case 'airflow' rbac_user is not present. Password is: 'airflow'"
-    python add_user.py
     exec airflow webserver
     ;;
   worker|scheduler)
