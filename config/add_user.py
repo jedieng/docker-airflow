1dba5f3d39431301aa7d96f0469b9a3f8d158ea3
Yurii Sokolenko <yurasokol3nko@gmail.com>
2019-01-30 18:55:29 +0200

    change auth method

--- config/add_user.py
+++ config/add_user.py
@@ -1,17 +1,17 @@
-from airflow.www_rbac.app import cached_appbuilder
+from airflow import models, settings
+from airflow.contrib.auth.backends.password_auth import PasswordUser
+from flask_bcrypt import generate_password_hash
+from sqlalchemy import exists
 
-username = 'airflow'
-firstname = 'Airflow'
-lastname = 'Admin'
-email = 'airflow@fab_airflow.com'
-role_str = 'Admin'
-password = 'airflow'
-
-appbuilder = cached_appbuilder()
-role = appbuilder.sm.find_role(role_str)
-if appbuilder.sm.find_user(username):
-    print('{} already exist in the db'.format(username))
-else:
-    user = appbuilder.sm.add_user(username, firstname, lastname, email, role, password)
-    if user:
-        print('{} user {} created.'.format(role, username))
+session = settings.Session()
+isUserPresent = not session.query(exists().where(models.User.username=='airflow')).scalar()
+if(isUserPresent):
+	user = PasswordUser(models.User())
+	user.username = 'airflow'
+	user.email = 'airflow@fab_airflow.com'
+	user.superuser = True
+	user._password = generate_password_hash('airflow', 12)
+	session.add(user)
+	session.commit()
+session.close()
+exit()
